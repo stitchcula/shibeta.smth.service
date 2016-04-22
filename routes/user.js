30522d5f33466940a59fe1d280c74ffb3c29099b
stitch cula <1132463097@qq.com>
2016-04-23 00:25:38 +0800

    debug

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,5 @@
 import Router from 'koa-router'
-import fetch from 'node-fetch'
+import request from 'co-request'
 import parse from 'co-body'
 
 const router=new Router()
@@ -7,20 +7,18 @@ const router=new Router()
 const proxyHost="http://iot.shibeta.org:806"
 
 router.get('/',async ctx=>{
-    var res=await fetch(proxyHost+ctx.path+"?"+ctx.querystring)
-    ctx.body=await res.json();
+    var res=await request({uri:proxyHost+ctx.path+"?"+ctx.querystring,method:'GET'})
+    ctx.body=res.body
 })
 
 router.post('/signup',async (ctx,next)=>{
-    var res=await fetch(proxyHost+ctx.path+"?"+ctx.querystring,
-        {method:"POST",body:await parse.json(ctx)})
-    res=await res.json()
-    if(res.result==200){
-        res2=await fetch(proxyHost+ctx.path+"?uin="+res.uin+"&token="+res.token,
-            {method:"POST"})
-        res2=await res2.json()
-        if(res2.result==200)
-            this.body=res
+    var res=await request({uri:proxyHost+ctx.path+"?"+ctx.querystring,
+        method:"POST",json:true,body:JSON.stringify(await parse.json(ctx))})
+    if(res.body.result==200){
+        var res2=await request({uri:proxyHost+"/user/oauth?uin="+res.body.uin+"&token="+res.body.token,
+            method:"POST",json:true})
+        if(res2.body.result==200)
+            ctx.body=res.body
     }else
         ctx.body=res
     await next()
@@ -28,7 +26,7 @@ router.post('/signup',async (ctx,next)=>{
 
 router.post('/login',async (ctx,next)=>{
     var res=await fetch(proxyHost+ctx.path+"?"+ctx.querystring,
-        {method:"POST",body:await parse.json(ctx)})
+        {method:"POST",body:JSON.stringify(await parse.json(ctx))})
     ctx.body=await res.json()
     await next();
 })
